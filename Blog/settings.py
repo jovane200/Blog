3245d85b694fc13fc49e06242fb30659a54ee1cb
Jovane <jovanepadonou@gmail.com>
2025-07-19 14:52:38 +0100

    Customize admin layout and favicon

--- Blog/settings.py
+++ Blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','unsafe-default-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-t8gj.onrender.com']
 
 
 # Application definition
