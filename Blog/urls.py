b6cbd66e320bce3f45aa3d72e967a8b2088c801b
Jovane <jovanepadonou@gmail.com>
2025-07-19 01:37:24 +0100

    Fix media serving for production

--- Blog/urls.py
+++ Blog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myblog/',include('myblog.urls',namespace='myblog')),
+    path('',include('myblog.urls',namespace='myblog')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
