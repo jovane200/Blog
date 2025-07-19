adf0d8f930dd08d91557a2f8272c9840ca1f1b25
Jovane <jovanepadonou@gmail.com>
2025-07-19 01:28:51 +0100

    Fix media serving for production

--- Blog/urls.py
+++ Blog/urls.py
@@ -24,9 +24,4 @@ urlpatterns = [
     path('myblog/',include('myblog.urls',namespace='myblog')),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
