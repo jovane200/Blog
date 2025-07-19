712de527db4502c2d995990253bb73f227d37e83
Jovane <jovanepadonou@gmail.com>
2025-07-19 06:06:15 +0100

    Fix media serving path for Render

--- Blog/urls.py
+++ Blog/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('',include('myblog.urls',namespace='myblog')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
